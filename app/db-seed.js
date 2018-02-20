f1df77da2afb98195ad8ac0703c590cf0b2adffb
Mark Ross <krazkidd@gmail.com>
2018-02-19 18:54:59 -0700

    Rearrange DB seed code.

--- app/db-seed.js
+++ app/db-seed.js
@@ -1,4 +1,57 @@
+var players1 = [
+  {
+    "name": "Terry Fallweather",
+    "number": "10"
+  },
+  {
+    "name": "Tera Splendor",
+    "number": "32"
+  },
+  {
+    "name": "Greg Butters",
+    "number": "85"
+  },
+  {
+    "name": "Norma Frent",
+    "number": "12"
+  },
+  {
+    "name": "Grelt Lazer",
+    "number": "99"
+  },
+  {
+    "name": "Greese Witherspoon",
+    "number": "11"
+  },
+  {
+    "name": "Booze Willis",
+    "number": "21"
+  },
+  {
+    "name": "Jen Stiller",
+    "number": "1"
+  },
+  {
+    "name": "Mike Wizekowski",
+    "number": "22"
+  },
+  {
+    "name": "Barbara La Barbara",
+    "number": "33"
+  }
+];
+
+var lineup1 =   {
+  "teamName": "Painkillers",
+  "players": players1,
+  "batPositions": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  "fieldPositions": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  "subs": []
+};
+
 module.exports = function (db) {
+
+  //TODO I feel like this could be (should be?) done in a single promise chain..
 
   db.Lineup.remove({}, err => {
     if (err) {
@@ -18,61 +71,10 @@ module.exports = function (db) {
     }
   });
 
-  var players = [
-    {
-      "name": "Terry Fallweather",
-      "number": "10"
-    },
-    {
-      "name": "Tera Splendor",
-      "number": "32"
-    },
-    {
-      "name": "Greg Butters",
-      "number": "85"
-    },
-    {
-      "name": "Norma Frent",
-      "number": "12"
-    },
-    {
-      "name": "Grelt Lazer",
-      "number": "99"
-    },
-    {
-      "name": "Greese Witherspoon",
-      "number": "11"
-    },
-    {
-      "name": "Booze Willis",
-      "number": "21"
-    },
-    {
-      "name": "Jen Stiller",
-      "number": "1"
-    },
-    {
-      "name": "Mike Wizekowski",
-      "number": "22"
-    },
-    {
-      "name": "Barbara La Barbara",
-      "number": "33"
-    }
-  ];
-
-  db.Player.create(players).then(players => {
+  db.Player.create(players1).then(players => {
     console.log("Players created.")
 
-    var lineupDb =   {
-        "teamName": "Painkillers",
-        "players": players,
-        "batPositions": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-        "fieldPositions": [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-        "subs": []
-      };
-
-    db.Lineup.create(lineupDb, (err, lineup) => {
+    db.Lineup.create(lineup1, (err, lineup) => {
       if (err) {
         console.log("Error: Lineup not created.");
         console.log(err);
