ef3faa382e8beacf0d4aa8c2601489ddb4f5fc01
Mark Ross <krazkidd@gmail.com>
2023-09-13 06:57:25 -0700

    Make licensing text stick to bottom of screen.

--- app.vue
+++ app.vue
@@ -2,13 +2,13 @@
 </script>
 
 <template>
-  <div class="flex flex-col items-center">
+  <div class="flex flex-col items-center min-h-screen">
     <!-- <nav class="flex items-end p-2 space-x-2">
     </nav> -->
 
-    <Lineup class="justify-center w-full md:w-3/4 xl:w-1/2 p-5" />
+    <Lineup class="grow justify-center w-full md:w-3/4 xl:w-1/2 p-5" />
 
-    <div class="text-sm text-gray-700 dark:text-gray-300 pt-10">
+    <div class="text-center text-sm text-gray-700 dark:text-gray-300 pt-10">
       This program is licensed to you for use under the <a href="https://www.gnu.org/licenses/agpl.html" class="underline">GNU AGPL v3</a>. <a href="https://github.com/krazkidd/lineup"><i class="pi pi-github" /></a>
     </div>
   </div>
@@ -18,5 +18,10 @@
 .dark body {
   background-color: #091a28;
   /* color: #ebf4f1; */
+}
+
+body,
+#__nuxt {
+  min-height: 100vh;
 }
 </style>
