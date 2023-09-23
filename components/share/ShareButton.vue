2870619ae964fef1e20150657bb9118773c7bac8
Mark Ross <krazkidd@gmail.com>
2023-09-22 17:48:40 -0700

    Add share button to show link to scoreboard.

---
+++ components/share/ShareButton.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { scoreboardMenuItem } from '~~/nav';
+import type { ID, Team } from '~~/types';
+
+const props = defineProps<{
+    teamId: ID
+    team: Team
+}>();
+
+const buttonPassThroughOptions = {
+  label: {
+    class: 'hidden'
+  }
+};
+
+const isDialogVisible = ref(false);
+</script>
+
+
+<template>
+    <Button
+        @click="isDialogVisible = true"
+        icon="pi pi-fw pi-share-alt"
+        severity="secondary"
+        text
+        rounded
+        aria-label="Share"
+        title="Share"
+        :pt="buttonPassThroughOptions"
+    />
+
+    <Dialog
+        v-model:visible="isDialogVisible"
+        modal
+        :header="`${team.name}'s Scoreboard`"
+        :pt="{
+            root: { class: 'w-full md:w-3/4 xl:w-1/2' },
+            content: { class: 'pt-1' }
+        }"
+    >
+        <RouterLink :to="scoreboardMenuItem.to!" v-slot="{ href, route, navigate, isActive, isExactActive }" custom>
+            <a :href="href.concat('/', props.teamId)" v-bind="props.action" @click="navigate">Link</a>
+        </RouterLink>
+    </Dialog>
+</template>
--- pages/scoreboard/[[id]].vue
+++ pages/scoreboard/[[id]].vue
@@ -21,72 +21,82 @@ const buttonPassThroughOptions = {
 </script>
 
 <template>
-    <div class="flex flex-col sm:flex-row justify-around items-center text-center">
-        <Card class="m-1 w-64">
-            <template #header>
-                <span class="text-6xl">{{ scoreboard?.teamScore }}</span>
-            </template>
-            <template #title>{{ team?.name }}</template>
-            <template #footer>
-                <div class="flex justify-around">
-                    <Button
-                        @click="incrementTeamScore(1)"
-                        icon="pi pi-caret-up"
-                        severity="success"
-                        rounded
-                        aria-label="Add Run"
-                        title="Add Run"
-                        :loading="pending"
-                        :pt="buttonPassThroughOptions"
-                    />
+    <div>
+        <div class="flex font-bold mb-4">
+            <div
+                class="grow inline-block px-1"
+            />
 
-                    <Button
-                        @click="incrementTeamScore(-1)"
-                        icon="pi pi-caret-down"
-                        severity="danger"
-                        rounded
-                        outlined
-                        aria-label="Remove Run"
-                        title="Remove Run"
-                        :loading="pending"
-                        :pt="buttonPassThroughOptions"
-                    />
-                </div>
-            </template>
-        </Card>
+            <ShareButton :teamId="teamId" :team="team!" />
+        </div>
 
-        <Card class="m-1 w-64">
-            <template #header>
-                <span class="text-6xl">{{ scoreboard?.otherTeamScore }}</span>
-            </template>
-            <template #title>Opposing Team</template>
-            <template #footer>
-                <div class="flex justify-around">
-                    <Button
-                        @click="incrementOtherTeamScore(1)"
-                        icon="pi pi-caret-up"
-                        severity="success"
-                        rounded
-                        aria-label="Add Run"
-                        title="Add Run"
-                        :loading="pending"
-                        :pt="buttonPassThroughOptions"
-                    />
+        <div class="flex flex-col sm:flex-row justify-around items-center text-center">
+            <Card class="m-1 w-64">
+                <template #header>
+                    <span class="text-6xl">{{ scoreboard?.teamScore }}</span>
+                </template>
+                <template #title>{{ team?.name }}</template>
+                <template #footer>
+                    <div class="flex justify-around">
+                        <Button
+                            @click="incrementTeamScore(1)"
+                            icon="pi pi-caret-up"
+                            severity="success"
+                            rounded
+                            aria-label="Add Run"
+                            title="Add Run"
+                            :loading="pending"
+                            :pt="buttonPassThroughOptions"
+                        />
 
-                    <Button
-                        @click="incrementOtherTeamScore(-1)"
-                        icon="pi pi-caret-down"
-                        severity="danger"
-                        rounded
-                        outlined
-                        aria-label="Remove Run"
-                        title="Remove Run"
-                        :loading="pending"
-                        :pt="buttonPassThroughOptions"
-                    />
-                </div>
-            </template>
-        </Card>
+                        <Button
+                            @click="incrementTeamScore(-1)"
+                            icon="pi pi-caret-down"
+                            severity="danger"
+                            rounded
+                            outlined
+                            aria-label="Remove Run"
+                            title="Remove Run"
+                            :loading="pending"
+                            :pt="buttonPassThroughOptions"
+                        />
+                    </div>
+                </template>
+            </Card>
+
+            <Card class="m-1 w-64">
+                <template #header>
+                    <span class="text-6xl">{{ scoreboard?.otherTeamScore }}</span>
+                </template>
+                <template #title>Opposing Team</template>
+                <template #footer>
+                    <div class="flex justify-around">
+                        <Button
+                            @click="incrementOtherTeamScore(1)"
+                            icon="pi pi-caret-up"
+                            severity="success"
+                            rounded
+                            aria-label="Add Run"
+                            title="Add Run"
+                            :loading="pending"
+                            :pt="buttonPassThroughOptions"
+                        />
+
+                        <Button
+                            @click="incrementOtherTeamScore(-1)"
+                            icon="pi pi-caret-down"
+                            severity="danger"
+                            rounded
+                            outlined
+                            aria-label="Remove Run"
+                            title="Remove Run"
+                            :loading="pending"
+                            :pt="buttonPassThroughOptions"
+                        />
+                    </div>
+                </template>
+            </Card>
+        </div>
     </div>
 </template>
 
