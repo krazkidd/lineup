63d56cf80bfc6ab5e1a61cfad958a407db95ef7e
Mark Ross <krazkidd@gmail.com>
2023-09-22 18:22:37 -0700

    Fix favicon.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,7 @@ export default defineNuxtConfig({
         {
           rel: 'icon',
           type: 'image/x-icon',
-          href: '/favicon/favicon.ico'
+          href: '/lineup/favicon/favicon.ico'
         }
       ]
     }
