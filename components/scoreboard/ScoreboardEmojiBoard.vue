8e6cf106132e96fa113188527c8660232796abe3
Mark Ross <krazkidd@gmail.com>
2023-09-28 17:29:29 -0700

    Add initial version of emoji reactions.

---
+++ components/scoreboard/ScoreboardEmojiBoard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { emojis } from '~~/data/emoji';
+
+const emit = defineEmits<{
+  (e: "emote", payload: string): void;
+}>();
+</script>
+
+<template>
+    <div class="grid grid-cols-4 md:grid-cols-8 lg:grid-cols-10 justify-items-center md:w-128 gap-1">
+        <ScoreboardEmojiButton
+            v-for="emoji of emojis"
+            :key="emoji"
+            :id="emoji"
+            @emote="emit('emote', $event)"
+        />
+    </div>
+</template>
--- pages/scoreboard/[[id]].vue
+++ pages/scoreboard/[[id]].vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import type { Team } from '~~/types';
+import type { Team, Emote } from '~~/types';
 
 import { useTeamStore } from '~~/stores/Team';
 import { getTeam } from '~~/db/Team'
 import { getScoreboard, incrementTeamScore, incrementOtherTeamScore } from '~~/db/Scoreboard'
+import { getEmotes, addEmote, subscribeToNewEmotes } from '~~/db/Emotes'
 
 const route = useRoute()
 const db = useFirestore();
@@ -15,6 +16,15 @@ const teamId = computed(() => route.params.id as string || teamStore.id);
 const { data: team } = useDocument(await getTeam(db, teamId.value));
 const { data: scoreboard } = useDocument(await getScoreboard(db, teamId.value));
 
+const { data: emotes } = useCollection(await getEmotes(db));
+
+const unsubscribe = subscribeToNewEmotes(teamId.value, (data) => {
+    //TODO need to chain emote animations and remove after timeout
+    currentEmote.value = data.emote;
+});
+
+const currentEmote = ref('');
+
 const fakeTeam = {
     name: "Opposing Team",
     //jerseyColor: "",
@@ -24,19 +34,21 @@ const fakeTeam = {
 
 <template>
     <div>
-        <div class="flex justify-end mb-1">
+        <div class="flex justify-end mb-1 px-4">
             <ScoreboardShareButton :teamId="teamId" :team="team!" class="justify-self-end" />
         </div>
 
-        <div class="flex flex-col sm:flex-row justify-around items-center text-center">
+        <div class="flex flex-col sm:flex-row justify-between items-center text-center mb-1 px-4">
             <ScoreboardTeamTile
                 :team="team"
                 :score="scoreboard?.teamScore ?? 0"
                 :show-buttons="!route.params.id"
                 @add-run="incrementTeamScore(1)"
                 @remove-run="incrementTeamScore(-1)"
-                class="m-1 w-64"
+                class="w-64"
             />
+
+            <ScoreboardEmojiButton :id="currentEmote" :class="{ invisible: !currentEmote }" />
 
             <ScoreboardTeamTile
                 :team="fakeTeam"
@@ -44,9 +56,14 @@ const fakeTeam = {
                 :show-buttons="!route.params.id"
                 @add-run="incrementOtherTeamScore(1)"
                 @remove-run="incrementOtherTeamScore(-1)"
-                class="m-1 w-64"
+                class="w-64"
             />
         </div>
+
+        <ScoreboardEmojiBoard
+            @emote="addEmote({ teamId, emote: $event, ticks: Date.now() } as Emote)"
+            class=" px-4"
+        />
     </div>
 </template>
 
